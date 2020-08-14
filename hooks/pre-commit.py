986b3c6eec6ddaae4c322a11226d89cbf738b895
Meena-git-tech <meena.jtech@gmail.com>
2020-08-14 15:36:30 +0530

    EOL

--- hooks/pre-commit.py
+++ hooks/pre-commit.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python
 import os
 error = 0
-for line in open("index.php"):
-	if line[-1] == ';':
-		error=0
-	else:
-		error=1
-if error == 1:
-	print("error")
+vfiles=os.listdir("C:\\xampp\\htdocs\\git-hooks")
+for file in vfiles:
+	for line in open(file):
+		print(line)
+# for root, dirs, files in os.walk("C:\\xampp\\htdocs\\git-hooks"):
+	# print(files)
+	# for file in files:
+		# for line in open(file):
+			# if line[-1] == ';':
+				# error=0
+			# else:
+				# error=1
+		# if error == 1:
+			# print("error")
