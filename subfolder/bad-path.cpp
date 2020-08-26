c8de9bb23de5b91d8883cdfc61ae99996d123700
Meena-git-tech <meena.jtech@gmail.com>
2020-08-26 10:43:03 +0530

    Test

--- subfolder/bad-path.cpp
+++
@@ -1,6 +0,0 @@
-// There should not be Dsrc nor Isrc in the include paths
-// The two following lines are non-compliant
-#include <path/to/weirDsrc/include.h>
-#include <path/to/saIsrc/include.h>
-#include <path/to/Dsrc/include.h>
-// Not an include directive, this is fine
