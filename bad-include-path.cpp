16f106ea121ea3660adbd46e04503b5c353d89c3
Meena-git-tech <meena.jtech@gmail.com>
2020-08-27 09:56:29 +0530

    Test

--- bad-include-path.cpp
+++ bad-include-path.cpp
@@ -1,7 +1,7 @@
 // There should not be Dsrc nor Isrc in the include paths
 // The two following lines are non-compliant
 #include <path/to/Dsrc/include.h>
-#include <path/to/Isrc/include.h>
+#include <path/to/Isrc/include.h>  
 
 // Other mentions of Dsrc or Isrc like in the examples below are OK
 // dir name contains Isrc or Dsrc but has other stuff before or after
